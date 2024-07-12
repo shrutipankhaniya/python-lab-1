eb34d775a76043c5bc4f43e6d64951d468e2a901
shrutipankhaniya <bca2022shruti1634@tnraocollege.org>
2024-07-12 17:20:35 +0530

    Add files via upload

---
+++ Calculate.py
@@ -0,0 +1,10 @@
+num1 = int(input("enter first number"))
+num2 = int(input("enter second number"))
+num3 = num1 + num2
+print(num3)
+num3 = num1 - num2
+print(num3)
+num3 = num1 * num2
+print(num3)
+num3 = num1 / num2
+print(num3)
---
+++ empname.py
@@ -0,0 +1,11 @@
+empname = ["maya" , "priyanka" , "tara"]
+empname.append("aarohi")
+print(empname)
+
+empname = ["maya" , "priyanka" , "tara"]
+empname.insert(1,"aarohi")
+print(empname)
+
+empname = ["maya" , "priyanka" , "tara"]
+empname.remove("priyanka")
+print(empname)
